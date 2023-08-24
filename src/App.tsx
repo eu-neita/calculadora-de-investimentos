cd272b57e6383d5c04a32c19b43da58f645f3fb2
eu-neita <neitass.music@gmail.com>
2023-08-24 20:11:47 -0300

    feat: add land page and states

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,28 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [, ]
+  const [percentagePerMonth, setPercentagePerMonth ] = useState<string>('')
+  const [valuePerMonth, setValuePerMonth ] = useState<string>('')
+  const [timePerMonth, setTimePerMonth ] = useState<string>('')
+  const [timePerYear, setTimePerYear ] = useState<string>('')
 
   return (
-
+    <div>
+      <h3>Calcule seus investimentos para o futuro :rocket:</h3>
+      <div>
+        <span>quanto o seu investimento vai render por mês?</span>
+        <input 
+        type='text' 
+        placeholder='insira a %'
+        ></input>
+        <span>qual valor deseja colocar todo mês?</span>
+        <input type='text' placeholder='insira o valor em reais'></input>
+        <span>quanto tempo?(deve existir ao menos 1 campo preenchido para ser calculado)</span>
+        <input type='text' placeholder='insira o valor em messes'></input>
+        <input type='text' placeholder='insira o valor em anos'></input>
+        <span>{resultado}</span>
+      </div>
+    </div>
   )
 }
 
